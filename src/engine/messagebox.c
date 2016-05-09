b57f014770e6629972e5473976033e344afaa44b
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2016-05-10 04:13:00 +0600

    removed "{" and "}" in switch-case's

--- src/engine/messagebox.c
+++ src/engine/messagebox.c
@@ -7,24 +7,18 @@ void mtrShowSimpleMessageBox(uint8_t type, char *title, char *message)
         case MTR_DMT_INFO:
         case MTR_DMT_NOTE:
         case MTR_DMT_DEBUG:
-        {
             MessageBox(0, message, title,
               MB_OK | MB_ICONINFORMATION | MB_TASKMODAL);
             break;
-        }
         case MTR_DMT_WARNING:
-        {
             MessageBox(0, message, title,
               MB_OK | MB_ICONWARNING | MB_TASKMODAL);
             break;
-        }
         case MTR_DMT_ERROR:
         case MTR_DMT_FATAL:
-        {
             MessageBox(0, message, title,
               MB_OK | MB_ICONERROR | MB_TASKMODAL);
             break;
-        }
         default:
             break;
     }
@@ -35,15 +29,11 @@ bool mtrShowYesNoMessageBox(char *title, char *message)
     switch (MessageBox(0, message, title, MB_YESNO | MB_ICONQUESTION | MB_TASKMODAL))
     {
         case IDYES:
-        {
             return true;
             break;
-        }
         case IDNO:
-        {
             return false;
             break;
-        }
     }
     return false;
 }
