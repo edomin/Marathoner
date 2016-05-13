930a92f65f6a033c8460e0af0f6b2dc64b67a091
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2016-05-13 12:27:18 +0600

    Added Makefile and header for sdl_gpu lua binding

--- src/screen/sdl_gpu_lua52/binding.h
+++ src/screen/sdl_gpu_lua52/binding.h
@@ -1,1 +1,15 @@
+#ifndef MTR_SCREEN_SDL_GPU_LUA52
+#define MTR_SCREEN_SDL_GPU_LUA52
 
+#define MTR_PLUGIN
+#include "marathoner.h"
+
+//#include "sdl_gpu/SDL_gpu.h"
+
+//typedef struct mtrScreen_t {
+//    GPU_Target* screen;
+//} mtrScreen_t;
+
+//mtrScreen_t *mtrScreen;
+
+#endif
