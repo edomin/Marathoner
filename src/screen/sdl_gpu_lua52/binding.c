bed8483ee794b62c02ac157ca8c35dbbc8b17a4c
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2016-05-13 15:00:33 +0600

    sdl_gpu_lua52 plugin updated
    1. Fixed create report crush.
    2. Added plugin Init function and binded functions.
    3. Added lua 5.2 headers
    4. Added typedefs for external functions

--- src/screen/sdl_gpu_lua52/binding.c
+++ src/screen/sdl_gpu_lua52/binding.c
@@ -8,7 +8,37 @@ __declspec(dllexport) mtrReport* __stdcall mtrCreateReport(void)
     report = malloc(sizeof(mtrReport));
     report->moduleID = "Screen_sdl_gpu_Lua52";
     report->prereqsCount = 2;
+    report->prereqs = malloc(sizeof(char *) * report->prereqsCount);
     report->prereqs[0] = "Screen_sdl_gpu";
     report->prereqs[1] = "Script_Lua52";
     return report;
+}
+
+__declspec(dllexport) void __stdcall mtrPluginInit(void)
+{
+    mtrScriptsRegisterFunction = (mtrScriptsRegisterFunctionFunc)mtrFindFunction("Script_Lua52", "mtrScriptsRegisterFunction");
+    mtrScriptsGetVm = (mtrScriptsGetVmFunc)mtrFindFunction("Script_Lua52", "mtrScriptsGetVm");
+
+    mtrScreenInit = (mtrScreenInitFunc)mtrFindFunction("Screen_sdl_gpu", "mtrScreenInit");
+    mtrScreenQuit = (mtrScreenQuitFunc)mtrFindFunction("Screen_sdl_gpu", "mtrScreenQuit");
+
+    mtrScriptsRegisterFunction(mtrSF_ScreenInit, "ScreenInit");
+    mtrScriptsRegisterFunction(mtrSF_ScreenQuit, "ScreenQuit");
+}
+
+int mtrSF_ScreenInit(lua_State* l)
+{
+    int w = lua_tonumber(mtrVm, 1); /* получаем 1-ый */
+                                         /* переданный параметр */
+    int h = lua_tonumber(mtrVm, 2);
+//    const char *title = lua_tostring(mtrVm, 3);
+//    bool fullscreen = lua_toboolean(mtrVm, 4);
+    mtrScreenInit(w, h);
+    return 0;
+}
+
+int mtrSF_ScreenQuit(lua_State* l)
+{
+    mtrScreenQuit();
+    return 0;
 }
--- src/screen/sdl_gpu_lua52/binding.h
+++ src/screen/sdl_gpu_lua52/binding.h
@@ -4,12 +4,27 @@
 #define MTR_PLUGIN
 #include "marathoner.h"
 
+#include "lua52/lua.h"
+#include "lua52/lualib.h"
+#include "lua52/lauxlib.h"
+
 //#include "sdl_gpu/SDL_gpu.h"
 
-//typedef struct mtrScreen_t {
-//    GPU_Target* screen;
-//} mtrScreen_t;
+lua_State *mtrVm;
 
-//mtrScreen_t *mtrScreen;
+typedef lua_State* (__stdcall * mtrScriptsGetVmFunc)(void);
+mtrScriptsGetVmFunc mtrScriptsGetVm;
+
+typedef void (__stdcall * mtrScriptsRegisterFunctionFunc)(lua_CFunction, char *);
+mtrScriptsRegisterFunctionFunc mtrScriptsRegisterFunction;
+
+typedef void (__stdcall * mtrScreenInitFunc)(uint16_t, uint16_t);
+mtrScreenInitFunc mtrScreenInit;
+
+typedef void (__stdcall * mtrScreenQuitFunc)(void);
+mtrScreenQuitFunc mtrScreenQuit;
+
+int mtrSF_ScreenInit(lua_State* l);
+int mtrSF_ScreenQuit(lua_State* l);
 
 #endif
