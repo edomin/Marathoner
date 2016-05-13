2b39c58379c7e07b57291deafade312baba1efc1
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2016-05-13 12:26:22 +0600

    fixed lua include directory

--- src/script/lua52/script.h
+++ src/script/lua52/script.h
@@ -4,9 +4,9 @@
 #define MTR_PLUGIN
 #include "marathoner.h"
 
-#include "lua/lua.h"
-#include "lua/lualib.h"
-#include "lua/lauxlib.h"
+#include "lua52/lua.h"
+#include "lua52/lualib.h"
+#include "lua52/lauxlib.h"
 #include "bool/bool.h"
 
 lua_State *mtrVm;
