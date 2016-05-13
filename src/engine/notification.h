312d3c16c5f504cea2c7c0498701bdf76d3cab40
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2016-05-13 12:22:01 +0600

    Notification consts moved from marathoner.h to notification.h

--- src/engine/notification.h
+++ src/engine/notification.h
@@ -7,6 +7,14 @@
 #include "log.h"
 #include "messagebox.h"
 
+/* Notification texts */
+#define MTR_NTT_INFO    "Info"
+#define MTR_NTT_NOTE    "Note"
+#define MTR_NTT_WARNING "Warning"
+#define MTR_NTT_ERROR   "Error"
+#define MTR_NTT_FATAL   "Fatal Error"
+#define MTR_NTT_DEBUG   "Debug"
+
 void __stdcall mtrNotify(char *message, uint8_t level, uint8_t messageType);
 
 #endif
