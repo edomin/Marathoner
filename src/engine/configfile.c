286851a1200c7022d979032cf30b2809498a24ae
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2016-05-17 16:06:56 +0600

    Configfile module of engine added

--- src/engine/configfile.c
+++ src/engine/configfile.c
@@ -1,2 +1,15 @@
 #include "configfile.h"
 
+void ConfigfileInit(void)
+{
+
+}
+
+char *mtrConfigfileReadString(char* filename, char *section, char *key, char *defaultValue)
+{
+    uint16_t valueLength;
+    char *result;
+    valueLength = ini_gets(section, key, defaultValue, mtrConfigfileTempbuf, sizearray(mtrConfigfileTempbuf), filename);
+    result = malloc(sizeof(char *) * valueLength + 1);
+    return strcpy(result, (const char *)mtrConfigfileTempbuf);
+}
--- src/engine/configfile.h
+++ src/engine/configfile.h
@@ -1,6 +1,14 @@
 #ifndef MTR_ENGINE_CONFIGFILE
 #define MTR_ENGINE_CONFIGFILE
 
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include "minINI/minIni.h"
 
+#define sizearray(a)  (sizeof(a) / sizeof((a)[0]))
+char mtrConfigfileTempbuf[65536];
+
+char *mtrConfigfileReadString(char* filename, char *section, char *key, char *defaultValue);
 
 #endif
