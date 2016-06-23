230df15b3b7c80b9ae433e277768fdd26ed23a2b
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2016-06-24 01:08:10 +0600

    sdl_gpu texture plugin updated
    1. Texture indeces subroutines changed to using indexkeeper functions.
    2. Added texture loading error message

--- src/texture/sdl_gpu/texture.c
+++ src/texture/sdl_gpu/texture.c
@@ -12,87 +12,57 @@ __declspec(dllexport) mtrReport* __stdcall mtrCreateReport(void)
     return report;
 }
 
-__declspec(dllexport) void __stdcall mtrTextureInit(uint16_t reservedCount)
+__declspec(dllexport) bool __stdcall mtrTextureInit(uint32_t dmSize,
+ uint32_t reservedCount)
 {
     mtrLogWrite("Initializing texture manager", 0, MTR_LMT_INFO);
-    reservedTextures = reservedCount;
-    /* flooring to nearest power of 2 */
-    reservedTextures = reservedTextures | (reservedTextures >> 1);
-    reservedTextures = reservedTextures | (reservedTextures >> 2);
-    reservedTextures = reservedTextures | (reservedTextures >> 4);
-    reservedTextures = reservedTextures | (reservedTextures >> 8);
-    reservedTextures = reservedTextures | (reservedTextures >> 16);
-    reservedTextures = reservedTextures - (reservedTextures >> 1);
-    texturesCount = 0;
-    texturesMap = 0;
-    mtrTexture = malloc(sizeof(mtrTexture_t) * reservedTextures);
-    if (mtrTexture == NULL)
-        mtrNotify("Unable to allocate memory for texture structures", 1,
-         MTR_LMT_ERROR);
+
+    mtrTextureKeeper = (mtrIndexkeeper_t *)mtrIndexkeeperCreate(dmSize,
+     reservedCount, sizeof(mtrTexture_t));
+    if (mtrTextureKeeper == NULL)
+    {
+        mtrNotify("Unable to initialize texture manager", 1, MTR_LMT_ERROR);
+        return false;
+    }
     else
-        mtrLogWrite_i("Memory for texture structures allocated. Reserved places: ",
-         0, MTR_LMT_INFO, reservedTextures);
-    mtrLogWrite("Texture manager initialized", 0, MTR_LMT_INFO);
+        mtrLogWrite("Texture manager initialized", 0, MTR_LMT_INFO);
+    return true;
 }
 
-__declspec(dllexport) uint16_t __stdcall mtrTextureLoad(char *filename)
+__declspec(dllexport) uint32_t __stdcall mtrTextureLoad(const char *filename)
 {
-    uint16_t freeIndex;
+    uint32_t      freeIndex;
+    mtrTexture_t *texture;
 
     mtrLogWrite_s("Loading texture", 0, MTR_LMT_INFO, filename);
-    freeIndex = mtrTextureGetFreeIndex();
-    mtrTexture[freeIndex].texture = GPU_LoadImage(image_file);
-    if (texture != NULL)
+    freeIndex = mtrIndexkeeperGetFreeIndex(mtrTextureKeeper);
+    mtrLogWrite_i("Found free index: ", 1, MTR_LMT_INFO, freeIndex);
+    texture = (mtrTexture_t *)(&((mtrTexture_t *)mtrTextureKeeper->data)[freeIndex]);
+    texture->texture = GPU_LoadImage(filename);
+    if (texture->texture != NULL)
     {
-        mtrTexture[freeIndex].name = malloc(sizeof(char) * (strlen(filename) +
-         1));
-        mtrTexture[freeIndex].name = strcpy(mtrTexture[freeIndex].name,
-         (const char *)filename);
-
+        texture->name = malloc(sizeof(char) * (strlen(filename) + 1));
+        texture->name = strcpy(texture->name, filename);
+        mtrLogWrite_s("Texture loaded", 0, MTR_LMT_INFO, filename);
+        return freeIndex;
     }
     else
-        ;
-}
-uint16_t mtrTextureGetFreeIndex(void)
-{
-    uint16_t i;
-    uint8_t  j;
-    uint16_t textureNum;
-    uint16_t mapNumberNum;
-    uint8_t  bitNum;
-    mtrTexture_t *temp;
-
-    /* Searching empty place on textures map */
-    for (i = 0; i < (reservedTextures >> 5); i++) /* reservedTextures >> 5 = */
-    {                                             /* reservedTextures / 32   */
-        if (texturesMap[i] < 4294967295) /* max uint32_t = 4294967296 */
-        {
-            for (j = 0; j < 32; j++)
-            {
-                if (((texturesMap[i] & ( 1 << j )) >> j) == 0)
-                {
-                    mapNumberNum = i;
-                    bitNum = j;
-                    textureNum = i << 5 + j; /* textureNum = i * 32 + j */
-                    break;
-                }
-            }
-            break;
-        }
-    }
-    /* if no empty space for texture structures, then we need allocate new
-     * array with length larger than old in 2 times */
-    if (textureNum > reservedTextures)
     {
-        temp = mtrTexture;
-        mtrTexture = malloc(sizeof(mtrTexture_t) * reservedTextures * 2);
-        for (i = 0; i < reservedTextures; i++)
-        {
-            mtrTexture[i].texture = temp[i].texture;
-            mtrTexture[i].filename = temp[i].filename;
-        }
-        free(temp);
-        reservedTextures = reservedTextures * 2;
+        mtrNotify("Unable to load texture", 1, MTR_LMT_ERROR);
+        mtrIndexkeeperFreeIndex(mtrTextureKeeper, freeIndex);
+        return 0;
     }
-    return textureNum;
+    return 0;
+}
+
+__declspec(dllexport) void __stdcall mtrTextureFree(uint32_t texNum)
+{
+    mtrTexture_t *texture;
+
+    texture = (mtrTexture_t *)(&((mtrTexture_t *)mtrTextureKeeper->data)[texNum]);
+    mtrLogWrite_s("Unloading texture", 0, MTR_LMT_INFO, texture->name);
+    free(texture->name);
+    GPU_FreeImage (texture->texture);
+    mtrIndexkeeperFreeIndex(mtrTextureKeeper, texNum);
+    mtrLogWrite("Texture unloaded", 0, MTR_LMT_INFO);
 }
--- src/texture/sdl_gpu/texture.h
+++ src/texture/sdl_gpu/texture.h
@@ -7,13 +7,10 @@
 #include "sdl_gpu/SDL_gpu.h"
 
 typedef struct mtrTexture_t {
-    GPU_Image texture;
-    const char *name;
+    GPU_Image *texture;
+    char *name;
 } mtrTexture_t;
 
-mtrTexture_t *mtrTexture;
-uint16_t reservedTextures;
-uint16_t texturesCount;
-uint32_t texturesMap[2048]; /* bit array */
+mtrIndexkeeper_t *mtrTextureKeeper;
 
 #endif
