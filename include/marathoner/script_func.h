5e888fa06e4dfc9e3661995ed4ccce2a81d26e03
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2016-10-17 03:07:13 +0600

    Created new API for script VMs' binding modules

---
+++ include/marathoner/script_func.h
@@ -0,0 +1,16 @@
+#ifdef lua_h
+    #define MTR_SCRIPT_FUNC(funcname)                int funcname(lua_State* l)
+
+    #define MTR_SF_GET_BOOL(varname, stackpos)       varname = lua_toboolean(mtrVm, stackpos)
+    #define MTR_SF_GET_INT(varname, stackpos)        varname = lua_tointeger(mtrVm, stackpos)
+    #define MTR_SF_GET_SINGLE(varname, stackpos)     varname = (float)lua_tonumber(mtrVm, stackpos)
+    #define MTR_SF_GET_DOUBLE(varname, stackpos)     varname = lua_tonumber(mtrVm, stackpos)
+    #define MTR_SF_GET_STRING(varname, stackpos)     varname = lua_tostring(mtrVm, stackpos)
+
+    #define MTR_SF_PUSH_BOOL(value)                  lua_pushboolean(mtrVm, value)
+    #define MTR_SF_PUSH_INT(value)                   lua_pushinteger(mtrVm, value)
+    #define MTR_SF_PUSH_SINGLE(value)                lua_pushnumber(mtrVm, (double)value)
+    #define MTR_SF_PUSH_DOUBLE(value)                lua_pushnumber(mtrVm, value)
+    #define MTR_SF_PUSH_STRING(value)                lua_pushstring(mtrVm, value)
+    #define MTR_SF_PUSH_NIL()                        lua_pushnil(mtrVm)
+#endif
