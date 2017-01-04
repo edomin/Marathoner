b59ba7edfafda52eb7770bf0b50b8a012e53eb11
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2017-01-04 21:30:26 +0600

    bugfix of the assemble sdl color with alpha from 32-bit color code

--- src/primitive/sdl2_gpu/primitive.h
+++ src/primitive/sdl2_gpu/primitive.h
@@ -25,9 +25,9 @@
     sdl_color.a = 0xFF;
 
 #define MTR_COLOR32_TO_SDL_COLOR(sdl_color, color)                          \
-    sdl_color.r = (uint8_t)(color >> 16);                                   \
-    sdl_color.g = (uint8_t)((color >> 8) - ((uint32_t)sdl_color.r << 8));   \
-    sdl_color.b = (uint8_t)(color - ((uint32_t)sdl_color.r << 16) -         \
+    sdl_color.r = (uint8_t)(color >> 24);                                   \
+    sdl_color.g = (uint8_t)((color >> 16) - ((uint32_t)sdl_color.r << 8));  \
+    sdl_color.b = (uint8_t)((color >> 8) - ((uint32_t)sdl_color.r << 16) -  \
      ((uint32_t)sdl_color.g << 8));                                         \
     sdl_color.a = (uint8_t)(color - ((uint32_t)sdl_color.r << 24) -         \
      ((uint32_t)sdl_color.g << 16) - ((uint32_t)sdl_color.b << 8));
