81cbf317332da86f6aa7d66ead2483e0ea86eeec
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2017-02-23 05:49:26 +0600

    function mtrEncodingUtf8ToUcs4 rewrited

--- src/core/encoding.c
+++ src/core/encoding.c
@@ -5,14 +5,16 @@ size_t MTR_CALL mtrEncodingUtf8ToUcs4(const char *utf8Text, uint32_t **ucs4Text)
     size_t len;
     size_t max;
     size_t n;
+    int illegal;
+
     if (utf8Text == NULL)
         return 0;
     len = strlen(utf8Text);
     max = len + 1;
-    ucs4Text = malloc(sizeof(uint32_t) * max);
-    if (ucs4Text == NULL)
+    *ucs4Text = malloc(sizeof(ucs4_t) * max);
+    if (*ucs4Text == NULL)
         return 0;
-    n = u8decode(utf8Text, (ucs4_t *)ucs4Text, max, NULL);
-    ucs4Text = realloc(ucs4Text, n);
+    illegal = 0;
+    n = u8decode(utf8Text, (ucs4_t *)*ucs4Text, max, &illegal);
     return n;
 }
