a1bb71bf3009fb715e892c5f4a9b5aa744b8ef4a
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2017-03-07 11:51:19 +0600

    Bugfix + more info in log added in function mtrSpriteLoad

--- src/abstraction/sprite/sprite.c
+++ src/abstraction/sprite/sprite.c
@@ -83,32 +83,39 @@ MTR_EXPORT uint32_t MTR_CALL mtrSpriteLoad(const char *filename, int clipWidth,
         sprite->name = strcpy(sprite->name, filename);
         /* TODO: Write actual values in log */
         mtrTextureGetSizes(sprite->textureIndex, &textureWidth, &textureHeight);
+        mtrLogWrite_i("Texture width:", 1, MTR_LMT_INFO, textureWidth);
+        mtrLogWrite_i("Texture height:", 1, MTR_LMT_INFO, textureHeight);
 
         if ((clipWidth != 0) && (clipWidth <= textureWidth))
             sprite->clipWidth = clipWidth;
         else
             sprite->clipWidth = textureWidth;
+        mtrLogWrite_i("Clip width:", 1, MTR_LMT_INFO, sprite->clipWidth);
 
         if ((clipHeight != 0) && (clipHeight <= textureHeight))
             sprite->clipHeight = clipHeight;
         else
             sprite->clipHeight = textureHeight;
+        mtrLogWrite_i("Clip height:", 1, MTR_LMT_INFO, sprite->clipHeight);
 
         if ((rowsCount == 0) || (rowsCount * sprite->clipWidth > textureWidth))
             sprite->rowsCount = textureWidth / sprite->clipWidth;
         else
             sprite->rowsCount = rowsCount;
+        mtrLogWrite_i("Rows count:", 1, MTR_LMT_INFO, sprite->rowsCount);
 
         if ((colsCount == 0) ||
          (colsCount * sprite->clipHeight > textureHeight))
             sprite->colsCount = textureHeight / sprite->clipHeight;
         else
             sprite->colsCount = colsCount;
+        mtrLogWrite_i("Cols count:", 1, MTR_LMT_INFO, sprite->colsCount);
 
         if (clipsCount == 0)
-        {
             sprite->clipsCount = sprite->rowsCount * sprite->colsCount;
-        }
+        else
+            sprite->clipsCount = clipsCount;
+        mtrLogWrite_i("Clips count:", 1, MTR_LMT_INFO, sprite->clipsCount);
 
         sprite->clip = malloc(sizeof(mtrClip_t) * sprite->clipsCount);
         if (sprite->clip == NULL)
@@ -124,7 +131,7 @@ MTR_EXPORT uint32_t MTR_CALL mtrSpriteLoad(const char *filename, int clipWidth,
         {
             sprite->clip[i].x = sprite->clipWidth * (fmodl(i, sprite->colsCount));
             sprite->clip[i].y = sprite->clipHeight *
-             (int)ceilf((float)i / sprite->colsCount);
+             (int)floorf((float)i / (float)sprite->colsCount);
             sprite->clip[i].w = sprite->clipWidth;
             sprite->clip[i].h = sprite->clipHeight;
         }
