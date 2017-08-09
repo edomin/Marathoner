cf70c122de17b1ddfa82f1bded231064efe79d23
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2017-08-10 00:39:37 +0600

    codestyle fixes in test/script/script.lua

--- test/script/script.lua
+++ test/script/script.lua
@@ -2,8 +2,10 @@
 FileWriteLineFast("test/output.txt", "Positive test", FM_WRITE);
 RegisterStringVariable("STRING_VARIABLE", "Hello");
 RegisterNumericVariable("NUMERIC_VARIABLE", 9000);
-FileWriteLineFast("test/output.txt", "STRING_VARIABLE: " .. STRING_VARIABLE, FM_APPEND);
-FileWriteLineFast("test/output.txt", "NUMERIC_VARIABLE: " .. NUMERIC_VARIABLE, FM_APPEND);
+FileWriteLineFast("test/output.txt", "STRING_VARIABLE: " .. STRING_VARIABLE,
+ FM_APPEND);
+FileWriteLineFast("test/output.txt", "NUMERIC_VARIABLE: " .. NUMERIC_VARIABLE,
+ FM_APPEND);
 
 -- Negative
 FileWriteLineFast("test/output.txt", "Negative test", FM_APPEND);
@@ -14,7 +16,8 @@ RegisterStringVariable("", "Hello");
 FileWriteLineFast("test/output.txt", "string var - nil name", FM_APPEND);
 RegisterStringVariable(nil, "Hello");
 
-FileWriteLineFast("test/output.txt", "string var - empty table as name", FM_APPEND);
+FileWriteLineFast("test/output.txt", "string var - empty table as name",
+ FM_APPEND);
 RegisterStringVariable({}, "Hello");
 
 FileWriteLineFast("test/output.txt", "string var - number as name", FM_APPEND);
@@ -26,7 +29,8 @@ RegisterStringVariable("STRING_VARIABLE_2", 9000);
 FileWriteLineFast("test/output.txt", "string var - nil as value", FM_APPEND);
 RegisterStringVariable("STRING_VARIABLE_3", nil);
 
-FileWriteLineFast("test/output.txt", "string var - empty table as value", FM_APPEND);
+FileWriteLineFast("test/output.txt", "string var - empty table as value",
+ FM_APPEND);
 RegisterStringVariable("STRING_VARIABLE_4", {});
 
 FileWriteLineFast("test/output.txt", "string var - empty name", FM_APPEND);
@@ -35,7 +39,8 @@ RegisterNumericVariable("", 9000);
 FileWriteLineFast("test/output.txt", "string var - nil name", FM_APPEND);
 RegisterNumericVariable(nil, 9000);
 
-FileWriteLineFast("test/output.txt", "string var - empty table as name", FM_APPEND);
+FileWriteLineFast("test/output.txt", "string var - empty table as name",
+ FM_APPEND);
 RegisterNumericVariable({}, 9000);
 
 FileWriteLineFast("test/output.txt", "string var - number as name", FM_APPEND);
@@ -47,19 +52,29 @@ RegisterNumericVariable("NUMERIC_VARIABLE_2", "Hello");
 FileWriteLineFast("test/output.txt", "string var - nil as value", FM_APPEND);
 RegisterNumericVariable("NUMERIC_VARIABLE_3", nil);
 
-FileWriteLineFast("test/output.txt", "string var - empty table as value", FM_APPEND);
+FileWriteLineFast("test/output.txt", "string var - empty table as value",
+ FM_APPEND);
 RegisterNumericVariable("NUMERIC_VARIABLE_4", {});
 
-FileWriteLineFast("test/output.txt", "STRING_VARIABLE_2: " .. STRING_VARIABLE_2, FM_APPEND);
-FileWriteLineFast("test/output.txt", "STRING_VARIABLE_3: " .. STRING_VARIABLE_3, FM_APPEND);
-FileWriteLineFast("test/output.txt", "STRING_VARIABLE_4: " .. STRING_VARIABLE_4, FM_APPEND);
-FileWriteLineFast("test/output.txt", "NUMERIC_VARIABLE_2: " .. NUMERIC_VARIABLE_2, FM_APPEND);
-FileWriteLineFast("test/output.txt", "NUMERIC_VARIABLE_3: " .. NUMERIC_VARIABLE_3, FM_APPEND);
-FileWriteLineFast("test/output.txt", "NUMERIC_VARIABLE_4: " .. NUMERIC_VARIABLE_4, FM_APPEND);
+FileWriteLineFast("test/output.txt", "STRING_VARIABLE_2: " .. STRING_VARIABLE_2,
+ FM_APPEND);
+FileWriteLineFast("test/output.txt", "STRING_VARIABLE_3: " .. STRING_VARIABLE_3,
+ FM_APPEND);
+FileWriteLineFast("test/output.txt", "STRING_VARIABLE_4: " .. STRING_VARIABLE_4,
+ FM_APPEND);
+FileWriteLineFast("test/output.txt",
+ "NUMERIC_VARIABLE_2: " .. NUMERIC_VARIABLE_2, FM_APPEND);
+FileWriteLineFast("test/output.txt",
+ "NUMERIC_VARIABLE_3: " .. NUMERIC_VARIABLE_3, FM_APPEND);
+FileWriteLineFast("test/output.txt",
+ "NUMERIC_VARIABLE_4: " .. NUMERIC_VARIABLE_4, FM_APPEND);
 
 -- Registering variable with same name
-FileWriteLineFast("test/output.txt", "Test of registering variable with same name", FM_APPEND);
+FileWriteLineFast("test/output.txt",
+ "Test of registering variable with same name", FM_APPEND);
 RegisterStringVariable("STRING_VARIABLE", "Hello");
 RegisterNumericVariable("NUMERIC_VARIABLE", 9000);
-FileWriteLineFast("test/output.txt", "STRING_VARIABLE: " .. STRING_VARIABLE, FM_APPEND);
-FileWriteLineFast("test/output.txt", "NUMERIC_VARIABLE: " .. NUMERIC_VARIABLE, FM_APPEND);
+FileWriteLineFast("test/output.txt", "STRING_VARIABLE: " .. STRING_VARIABLE,
+ FM_APPEND);
+FileWriteLineFast("test/output.txt", "NUMERIC_VARIABLE: " .. NUMERIC_VARIABLE,
+ FM_APPEND);
