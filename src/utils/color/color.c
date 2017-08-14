f1dea4a7a17a37b1ade5bd9f9f0254df1bebd1cd
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2017-08-15 00:07:46 +0600

    Small fixes in bitvise operations in color module

--- src/utils/color/color.c
+++ src/utils/color/color.c
@@ -21,21 +21,23 @@ MTR_EXPORT mtrReport* MTR_CALL mtrCreateReport(void)
 MTR_EXPORT uint32_t MTR_CALL mtrColorAssembleRGB(uint8_t r, uint8_t g,
  uint8_t b)
 {
-    return ((uint32_t)r << 16) + ((uint32_t)g << 8) + b;
+    return ((uint32_t)r << 16) | ((uint32_t)g << 8) | b;
 }
 
 MTR_EXPORT uint32_t MTR_CALL mtrColorAssembleRGBA(uint8_t r, uint8_t g,
  uint8_t b, uint8_t a)
 {
-    return ((uint32_t)r << 24) + ((uint32_t)g << 16) + ((uint32_t)b << 8) + a;
+    return ((uint32_t)r << 24) | ((uint32_t)g << 16) | ((uint32_t)b << 8) | a;
 }
 
 MTR_EXPORT void MTR_CALL mtrColorSplitRGB(uint32_t rgb, uint8_t *r, uint8_t *g,
  uint8_t *b)
 {
-    *r = (uint8_t)(rgb >> 16);
-    *g = (uint8_t)((rgb >> 8) - ((uint32_t)(*r) << 8));
-    *b = (uint8_t)(rgb - ((uint32_t)(*r) << 16) - ((uint32_t)(*g) << 8));
+    *r = (uint8_t)((rgb & 0xFF0000) > 16);
+    *g = (uint8_t)((rgb & 0x00FF00) > 8);
+    *b = (uint8_t)(rgb & 0x0000FF);
+}
+
 MTR_EXPORT uint8_t MTR_CALL mtrColorRGBGetR(uint32_t rgb)
 {
     return (rgb & 0xFF0000) >> 16;
@@ -69,11 +71,12 @@ MTR_EXPORT uint32_t MTR_CALL mtrColorRGBSetB(uint32_t rgb, uint8_t b)
 MTR_EXPORT void MTR_CALL mtrColorSplitRGBA(uint32_t rgba, uint8_t *r,
  uint8_t *g, uint8_t *b, uint8_t *a)
 {
-    *r = (uint8_t)(rgba >> 24);
-    *g = (uint8_t)((rgba >> 16) - ((uint32_t)(*r) << 8));
-    *b = (uint8_t)((rgba >> 8) - ((uint32_t)(*r) << 16) - ((uint32_t)(*g) << 8));
-    *a = (uint8_t)(rgba  - ((uint32_t)(*r) << 24) - ((uint32_t)(*g) << 16) -
-     ((uint32_t)(*b) << 8));
+    *r = (uint8_t)((rgba & 0xFF000000) >> 24);
+    *g = (uint8_t)((rgba & 0x00FF0000) >> 16);
+    *b = (uint8_t)((rgba & 0x0000FF00) >> 8);
+    *a = (uint8_t)(rgba & 0x000000FF);
+}
+
 MTR_EXPORT uint8_t MTR_CALL mtrColorRGBAGetR(uint32_t rgba)
 {
     return (rgba & 0xFF000000) >> 24;
@@ -116,7 +119,7 @@ MTR_EXPORT uint32_t MTR_CALL mtrColorRGBASetA(uint32_t rgba, uint8_t a)
 
 MTR_EXPORT uint32_t MTR_CALL mtrColorRGBtoRGBA(uint32_t rgb)
 {
-    return (rgb << 8) + 0xFF;
+    return (rgb << 8) | 0x000000FF;
 }
 
 MTR_EXPORT uint32_t MTR_CALL mtrColorRGBAtoRGB(uint32_t rgba)
