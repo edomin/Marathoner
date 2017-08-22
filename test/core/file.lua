b771e6649ef4c84dbbca3b3131194d49fdbd37df
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2017-08-22 22:17:55 +0600

    Finished test for file module of core

--- test/core/file.lua
+++ test/core/file.lua
@@ -1,3 +1,19 @@
+local size;
+local buffer;
 -- Positive
-FileWriteLineFast("test/output.txt", "Positive test", FM_WRITE);
--- TODO
+FileWriteLineFast("test/output.txt", "Positive", FM_WRITE);
+FileWriteFast("test/output.txt", "test", FM_APPEND);
+FileWriteLineFast("test/output.txt", " ", FM_APPEND);
+
+local file = FileOpen("test/output_2.txt", FM_WRITE);
+FileWriteLine(file, "Hello");
+FileWrite(file, "World");
+FileClose(file);
+file = FileOpen("test/output_2.txt", "FM_READ");
+if file ~= 0 then
+    FileWriteLineFast("test/output.txt", "File opened for read", FM_APPEND);
+end;
+buffer, size = FileRead(file);
+FileWriteLineFast("test/output.txt", size, FM_APPEND);
+FileWriteLineFast("test/output.txt", buffer, FM_APPEND);
+FileClose(file);
