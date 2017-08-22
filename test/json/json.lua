bc7867fad9335d4b39a5e451f7821ffcc9277dac
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2017-08-22 22:19:40 +0600

    Some fixed sjon test. I do not know, how to do valid usage scenery

--- test/json/json.lua
+++ test/json/json.lua
@@ -5,9 +5,16 @@ local success = JsonInit(IKDM_SMALL, 32);
 if success then
     FileWriteLineFast("test/output.txt", "Success", FM_APPEND);
     local json = JsonParseFile("test/json/test.json");
-    local parsedString = JsonParseString("{\"bool\": \"true\"}");
+    if json == 0 then
+        FileWriteLineFast("test/output.txt", "Failed to parse file", FM_APPEND);
+    end;
+
+    local parsedString = JsonParseString("{\"bool\": true}");
+
     local object = JsonCreateObject();
+
     local array = JsonCreateArray();
+
     JsonAddItemToObject(json, "object", object);
     JsonAddItemToObject(json, "array", array);
     JsonAddStringToObject(json, "string2", "world");
@@ -19,21 +26,22 @@ if success then
     JsonAddDoubleToArray(array, 142.2);
     JsonAddBoolToArray(array, true);
     JsonAddNullToArray(array);
-    local objectItem = JsonGetObjectItem(json, array);
+    FileWriteLineFast("test/output.txt", JsonToStringFormatted(json), FM_APPEND);
+    local objectItem = JsonGetObjectItem(json, "array");
     local objectItemString = JsonToString(objectItem);
     FileWriteLineFast("test/output.txt", objectItemString, FM_APPEND);
-    local arrayItem = JsonGetArrayItem(array, parsedString);
+    local arrayItem = JsonGetArrayItem(array, 0);
     local arrayItemStringFormatted = JsonToStringFormatted(arrayItem);
     FileWriteLineFast("test/output.txt", arrayItemStringFormatted, FM_APPEND);
     JsonToFile(json, "test/output.json");
     JsonToFileFormatted(json, "test/output_formatted.json");
 
-    JsonDelete(uint32_t entity);
+    JsonDelete(parsedString);
     JsonDeleteItemFromObject(json, "object");
-    JsonDeleteItemFromArray(array, 0);
+    --JsonDeleteItemFromArray(array, 1);
     JsonDetachItemFromObject(json, array);
     JsonDetachItemFromArray(array, 0);
-    if JsonObjectHasItem(object, "array")
+    if JsonObjectHasItem(object, "array") then
         FileWriteLineFast("test/output.txt", "Ok", FM_APPEND);
     else
         FileWriteLineFast("test/output.txt", "Not ok", FM_APPEND);
@@ -43,7 +51,7 @@ if success then
      FM_APPEND);
 
     JsonToFileFormatted(json, "test/output_formatted_json.json");
-    JsonToFileFormatted(array, "test/output_formatted_array.json");
+    --JsonToFileFormatted(array, "test/output_formatted_array.json");
 else
     FileWriteLineFast("test/output.txt", "Fail", FM_APPEND);
 end;
