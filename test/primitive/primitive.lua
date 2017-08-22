8619d75c988878b8ddce412e06ac71c119fd8376
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2017-08-22 22:20:16 +0600

    Updated primitive test

--- test/primitive/primitive.lua
+++ test/primitive/primitive.lua
@@ -6,8 +6,10 @@ local helicopter;
 ScreenInit(640, 480, false, "Primitive Test");
 TimerInit();
 PrimitiveInit();
+MouseInit();
 
 for i = 0, 30, 1 do
+    MouseRefresh();
     TimerStart();
     PrimitiveFill_c(0x000000);
     ScreenFlip();
@@ -15,6 +17,7 @@ for i = 0, 30, 1 do
 end;
 
 for i = 0, 30, 1 do
+    MouseRefresh();
     TimerStart();
     PrimitiveFill_ca(0xFF0000FF);
     ScreenFlip();
@@ -22,6 +25,7 @@ for i = 0, 30, 1 do
 end;
 
 for i = 0, 30, 1 do
+    MouseRefresh();
     TimerStart();
     PrimitiveFill_rgb(0x00, 0xFF, 0x00);
     ScreenFlip();
@@ -29,6 +33,7 @@ for i = 0, 30, 1 do
 end;
 
 for i = 0, 30, 1 do
+    MouseRefresh();
     TimerStart();
     PrimitiveFill_rgb(0x00, 0x00, 0xFF, 0x00);
     ScreenFlip();
@@ -36,6 +41,7 @@ for i = 0, 30, 1 do
 end;
 
 for i = 0, 60, 1 do
+    MouseRefresh();
     TimerStart();
     PrimitiveFill_c(0x000000);
     PrimitivePixel_rgb_f(32 + i * 2, 32, 0xFF, 0x00, 0x00);
@@ -47,6 +53,7 @@ for i = 0, 60, 1 do
 end;
 
 for i = 0, 60, 1 do
+    MouseRefresh();
     TimerStart();
     PrimitiveFill_c(0x000000);
     PrimitiveLine_rgb_f(32 + i * 2, 32, 32 + i * 2 + 4, 36, 0xFF, 0x00, 0x00);
@@ -59,6 +66,7 @@ for i = 0, 60, 1 do
 end;
 
 for i = 0, 60, 1 do
+    MouseRefresh();
     TimerStart();
     PrimitiveFill_c(0x000000);
     PrimitiveArc_rgb_f(32, 32, 24, i, i + 90, 0xFF, 0x00, 0x00);
@@ -70,6 +78,7 @@ for i = 0, 60, 1 do
 end;
 
 for i = 0, 60, 1 do
+    MouseRefresh();
     TimerStart();
     PrimitiveFill_c(0x000000);
     PrimitiveSegmentFilled_rgb_f(32, 32, 24, i, i + 90, 0xFF, 0x00, 0x00);
@@ -82,6 +91,7 @@ for i = 0, 60, 1 do
 end;
 
 for i = 0, 60, 1 do
+    MouseRefresh();
     TimerStart();
     PrimitiveFill_c(0x000000);
     PrimitiveCircle_rgb_f(32, 32, i, 0xFF, 0x00, 0x00);
@@ -93,6 +103,7 @@ for i = 0, 60, 1 do
 end;
 
 for i = 0, 60, 1 do
+    MouseRefresh();
     TimerStart();
     PrimitiveFill_c(0x000000);
     PrimitiveCircleFilled_rgb_f(32, 32, i, 0xFF, 0x00, 0x00);
@@ -104,6 +115,7 @@ for i = 0, 60, 1 do
 end;
 
 for i = 0, 60, 1 do
+    MouseRefresh();
     TimerStart();
     PrimitiveFill_c(0x000000);
     PrimitiveEllipse_rgb_f(32, 32, i, i / 2, i, 0xFF, 0x00, 0x00);
@@ -115,6 +127,7 @@ for i = 0, 60, 1 do
 end;
 
 for i = 0, 60, 1 do
+    MouseRefresh();
     TimerStart();
     PrimitiveFill_c(0x000000);
     PrimitiveEllipseFilled_rgb_f(32, 32, i, i / 2, i, 0xFF, 0x00, 0x00);
@@ -126,6 +139,7 @@ for i = 0, 60, 1 do
 end;
 
 for i = 0, 60, 1 do
+    MouseRefresh();
     TimerStart();
     PrimitiveFill_c(0x000000);
     PrimitiveRingSegment_rgb_f(32, 32, i / 60 * 24, 24, i, i + 90, 0xFF, 0x00,
@@ -139,6 +153,7 @@ for i = 0, 60, 1 do
 end;
 
 for i = 0, 60, 1 do
+    MouseRefresh();
     TimerStart();
     PrimitiveFill_c(0x000000);
     PrimitiveRingSegmentFilled_rgb_f(32, 32, i / 60 * 24, 24, i, i + 90, 0xFF,
@@ -154,6 +169,7 @@ for i = 0, 60, 1 do
 end;
 
 for i = 0, 60, 1 do
+    MouseRefresh();
     TimerStart();
     PrimitiveFill_c(0x000000);
     PrimitiveTriangle_rgb_f(32 + i * 2, 32, 32 + i * 2 + 32, 32,
@@ -169,6 +185,7 @@ for i = 0, 60, 1 do
 end;
 
 for i = 0, 60, 1 do
+    MouseRefresh();
     TimerStart();
     PrimitiveFill_c(0x000000);
     PrimitiveTriangleFilled_rgb_f(32 + i * 2, 32, 32 + i * 2 + 32, 32,
@@ -184,71 +201,77 @@ for i = 0, 60, 1 do
 end;
 
 for i = 0, 60, 1 do
+    MouseRefresh();
     TimerStart();
     PrimitiveFill_c(0x000000);
     PrimitiveRectangle_rgb_f(32 + i * 2, 32, 32 + i * 2 + 24, 56, 0xFF, 0x00,
      0x00);
     PrimitiveRectangle_rgba_f(32 + i * 2, 64, 32 + i * 2 + 24, 88, 0xFF, 0x00,
      0x00, 0xFF);
-    PrimitiveRectangle_c_f(32 + i * 2, 32, 96 + i * 2 + 24, 120, 0xFF0000);
-    PrimitiveRectangle_ca_f(32 + i * 2, 32, 128 + i * 2 + 24, 152, 0xFF0000FF);
+    PrimitiveRectangle_c_f(32 + i * 2, 96, 32 + i * 2 + 24, 120, 0xFF0000);
+    PrimitiveRectangle_ca_f(32 + i * 2, 128, 32 + i * 2 + 24, 152, 0xFF0000FF);
     ScreenFlip();
     TimerDelayForFPS(30);
 end;
 
 for i = 0, 60, 1 do
+    MouseRefresh();
     TimerStart();
     PrimitiveFill_c(0x000000);
     PrimitiveRectangleFilled_rgb_f(32 + i * 2, 32, 32 + i * 2 + 24, 56, 0xFF,
      0x00, 0x00);
     PrimitiveRectangleFilled_rgba_f(32 + i * 2, 64, 32 + i * 2 + 24, 88, 0xFF,
      0x00, 0x00, 0xFF);
-    PrimitiveRectangleFilled_c_f(32 + i * 2, 32, 96 + i * 2 + 24, 120,
+    PrimitiveRectangleFilled_c_f(32 + i * 2, 96, 32 + i * 2 + 24, 120,
      0xFF0000);
-    PrimitiveRectangleFilled_ca_f(32 + i * 2, 32, 128 + i * 2 + 24, 152,
+    PrimitiveRectangleFilled_ca_f(32 + i * 2, 128, 32 + i * 2 + 24, 152,
      0xFF0000FF);
     ScreenFlip();
     TimerDelayForFPS(30);
 end;
 
 for i = 0, 60, 1 do
+    MouseRefresh();
     TimerStart();
     PrimitiveFill_c(0x000000);
     PrimitiveRoundedRectangle_rgb_f(32 + i * 2, 32, 32 + i * 2 + 24, 56,
      i / 60 * 10, 0xFF, 0x00, 0x00);
     PrimitiveRoundedRectangle_rgba_f(32 + i * 2, 64, 32 + i * 2 + 24, 88,
      i / 60 * 10, 0xFF, 0x00, 0x00, 0xFF);
-    PrimitiveRoundedRectangle_c_f(32 + i * 2, 32, 96 + i * 2 + 24, 120,
+    PrimitiveRoundedRectangle_c_f(32 + i * 2, 96, 32 + i * 2 + 24, 120,
      i / 60 * 10, 0xFF0000);
-    PrimitiveRoundedRectangle_ca_f(32 + i * 2, 32, 128 + i * 2 + 24, 152,
+    PrimitiveRoundedRectangle_ca_f(32 + i * 2, 128, 32 + i * 2 + 24, 152,
      i / 60 * 10, 0xFF0000FF);
     ScreenFlip();
     TimerDelayForFPS(30);
 end;
 
 for i = 0, 60, 1 do
+    MouseRefresh();
     TimerStart();
     PrimitiveFill_c(0x000000);
     PrimitiveRoundedRectangleFilled_rgb_f(32 + i * 2, 32, 32 + i * 2 + 24, 56,
      i / 60 * 10, 0xFF, 0x00, 0x00);
     PrimitiveRoundedRectangleFilled_rgba_f(32 + i * 2, 64, 32 + i * 2 + 24, 88,
      i / 60 * 10, 0xFF, 0x00, 0x00, 0xFF);
-    PrimitiveRoundedRectangleFilled_c_f(32 + i * 2, 32, 96 + i * 2 + 24, 120,
+    PrimitiveRoundedRectangleFilled_c_f(32 + i * 2, 96, 32 + i * 2 + 24, 120,
      i / 60 * 10, 0xFF0000);
-    PrimitiveRoundedRectangleFilled_ca_f(32 + i * 2, 32, 128 + i * 2 + 24, 152,
+    PrimitiveRoundedRectangleFilled_ca_f(32 + i * 2, 128, 32 + i * 2 + 24, 152,
      i / 60 * 10, 0xFF0000FF);
     ScreenFlip();
     TimerDelayForFPS(30);
 end;
 
 for i = 0, 60, 1 do
+    MouseRefresh();
     TimerStart();
     PrimitiveFill_c(0x000000);
     PrimitiveLineBegin_f(32 + i, 32);
     PrimitiveLineTo_rgb_f(64 + i, 32, 0xFF, 0x00, 0x00);
-    PrimitiveLineTo_rgba_f(64 + i, 64, 0x00, 0xFF, 0x00, 0x00);
+    PrimitiveLineTo_rgba_f(64 + i, 64, 0x00, 0xFF, 0x00, 0xFF);
     PrimitiveLineTo_c_f(32 + i, 64, 0x0000FF);
     PrimitiveLineTo_ca_f(32 + i, 32, 0xFFFF00FF);
-    ScreenFlip();
     PrimitiveLineEnd();
+    ScreenFlip();
+    TimerDelayForFPS(30);
 end;
