3d6bc5b2a04d4258b40d640b0262028e525afccc
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2017-09-28 22:12:07 +0600

    Refactored Bindink.mk

--- Binding.mk
+++ Binding.mk
@@ -11,12 +11,13 @@ endif
 ifeq ($(PLATFORM), html5)
   LDFLAGS += -shared -Wl,--kill-at -L$(PREFIX)/lib -s SIDE_MODULE=1 -O2
 endif
-OBJSDIR = ../../../obj
-LIBDIR = ../../../lib
-BINDIR = ../../../plugin
-SRCDIR = ../../../src
-RCDIR = ../../../rc
-INCDIRS = -I$(PREFIX)/include -I../../../include $(XINCDIRS)
+ROOTDIR = ../../..
+OBJSDIR = $(ROOTDIR)/obj
+LIBDIR = $(ROOTDIR)/lib
+BINDIR = $(ROOTDIR)/plugin
+SRCDIR = $(ROOTDIR)/src
+RCDIR = $(ROOTDIR)/rc
+INCDIRS = -I$(PREFIX)/include -I$(ROOTDIR)/include $(XINCDIRS)
 
 STATIC_NAME = $(A_PR)mtr_$(MODULE_NAME)$(A_EXT)
 PLUGIN_NAME = $(SO_PR)mtr_$(MODULE_NAME)$(SO_EXT)
@@ -25,8 +26,8 @@ OBJ = $(OBJSDIR)/$(SUBSYSTEM)/$(MODULE).o
 RES =
 
 ifeq ($(PLATFORM), win32)
-  RCGEN = ../../../rcgen
-  VERSION_H = ../../../include/marathoner/version.h
+  RCGEN = $(ROOTDIR)/rcgen
+  VERSION_H = $(ROOTDIR)/include/marathoner/version.h
   RCFILE = $(RCDIR)/$(MODULE_NAME).rc
   RES = $(OBJSDIR)/$(SUBSYSTEM)/$(MODULE).res
   OBJS = $(OBJ) $(RES)
@@ -50,21 +51,20 @@ $(BINDIR)/$(PLUGIN_NAME): $(OBJS)
 	$(LD) $(LDFLAGS) -o $(BINDIR)/$(PLUGIN_NAME) $(OBJS) $(LIBS)
 
 $(OBJ): binding.c binding.h $(PREREQS) \
- ../../../include/marathoner/script_func.h \
- ../../../include/marathoner/version.h \
- ../../../include/marathoner/binding_common.h
+ $(ROOTDIR)/include/marathoner/script_func.h \
+ $(ROOTDIR)/include/marathoner/version.h
 	-mkdir $(OBJSDIR)/$(SUBSYSTEM)
 	$(CC) $(CFLAGS) $(INCDIRS) -c binding.c -o $(OBJ)
 
 $(RES): $(RCFILE)
 	$(RC) $(RCFLAGS) -i $(RCFILE) -o $(RES)
 
-$(RCFILE): ../../../include/marathoner/version.h
+$(RCFILE): $(ROOTDIR)/include/marathoner/version.h
 	$(RCGEN) $(VERSION_H) $(RCFILE) $(CONST_VER) $(MODULE_NAME) \
 	 $(PLUGIN_DESCRIPTION) $(PLUGIN_NAME)
 
-binding.c: ../../../include/marathoner/plugin_common.c
+binding.c: $(ROOTDIR)/include/marathoner/plugin_common.c
 
-binding.h: ../../../include/marathoner/plugin.h
+binding.h: $(ROOTDIR)/include/marathoner/plugin.h
 
-../../../include/marathoner/plugin.h: ../../../include/marathoner/marathoner.h
+$(ROOTDIR)/include/marathoner/plugin.h: $(ROOTDIR)/include/marathoner/marathoner.h
