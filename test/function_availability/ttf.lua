e2fd7e2bd35198b6fea6f558c7c6b9800641c75a
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2017-12-02 04:07:08 +0600

    Added missing functions in fa test of ttf

--- test/function_availability/ttf.lua
+++ test/function_availability/ttf.lua
@@ -6,7 +6,12 @@ fa.FunctionSupported(TtfInit, "MTR_TtfInit", TtfFunctionSupported);
 fa.FunctionSupported(TtfQuit, "MTR_TtfQuit", TtfFunctionSupported);
 fa.FunctionSupported(TtfLoad, "MTR_TtfLoad", TtfFunctionSupported);
 fa.FunctionSupported(TtfFree, "MTR_TtfFree", TtfFunctionSupported);
+fa.FunctionSupported(TtfGetWidth, "MTR_TtfGetWidth", TtfFunctionSupported);
+fa.FunctionSupported(TtfGetHeight, "MTR_TtfGetHeight", TtfFunctionSupported);
+fa.FunctionSupported(TtfGetSizes, "MTR_TtfGetSizes", TtfFunctionSupported);
 fa.FunctionSupported(TtfSetSizes, "MTR_TtfSetSizes", TtfFunctionSupported);
+fa.FunctionSupported(TtfGetStyle, "MTR_TtfGetStyle", TtfFunctionSupported);
+fa.FunctionSupported(TtfGetOutline, "MTR_TtfGetOutline", TtfFunctionSupported);
 fa.FunctionSupported(TtfSetStyle, "MTR_TtfSetStyle", TtfFunctionSupported);
 fa.FunctionSupported(TtfSetOutline, "MTR_TtfSetOutline", TtfFunctionSupported);
 fa.FunctionSupported(TtfGetGlyphSizes, "MTR_TtfGetGlyphSizes",
