5fd4b39fee35fe5e2976d1e3227b924574a71b5f
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2018-02-04 01:10:20 +0600

    Added types for some std file i/o functions

--- include/marathoner/types.h
+++ include/marathoner/types.h
@@ -1,7 +1,20 @@
 #ifndef MTR_TYPES_H
 #define MTR_TYPES_H
 
+/* std integer and boolean types */
 #include <stdint.h>
 #include <stdbool.h>
+
+/* FILE* type */
+#include <stdio.h>
+
+typedef FILE *(MTR_STDCALL *MTR_FopenFunc)(const char *, const char *);
+typedef int (MTR_STDCALL *MTR_FcloseFunc)(FILE *);
+typedef int (MTR_STDCALL *MTR_FeofFunc)(FILE *);
+typedef long int (MTR_STDCALL *MTR_FtellFunc)(FILE *);
+typedef int (MTR_STDCALL *MTR_FseekFunc)(FILE *, long int, int);
+typedef size_t (MTR_STDCALL *MTR_FreadFunc)(void *, size_t, size_t, FILE *);
+typedef size_t (MTR_STDCALL *MTR_FwriteFunc)(const void *, size_t, size_t,
+ FILE *);
 
 #endif
